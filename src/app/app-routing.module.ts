54f11e62bf5cdcd31676668abae426c2107525dc
dejene tesfaye <dejenet@tnt.com>
2025-04-12 14:26:49 +0300

    fix conflict

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CandidateComponent } from './candidate/candidate.component';
+import { SalaryComponent } from './salary/salary.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { DepartmentComponent } from './department/department.component';
+import { OrganizationComponent } from './organization/organization.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'candidate', component: CandidateComponent },
+  { path: 'salary', component: SalaryComponent },
+  { path: 'employee', component: EmployeeComponent },
+  { path: 'department', component: DepartmentComponent },
+  { path: 'organization', component: OrganizationComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
